59ef62a8e4055546faa5c7b468e6b88697ce84b7
Sheng Yunzhou <shengyunzhou@gmail.com>
2019-01-03 08:46:21 +0800

    add temporary solution

--- test/advent_of_code/day_three_test.clj
+++ test/advent_of_code/day_three_test.clj
@@ -1,17 +1,38 @@
 (ns advent-of-code.day-three-test
-  (:require [clojure.test :refer :all]
-            [clojure.string :as str]))
+  (:require [clojure.java.io :as io])
+  (:gen-class))
 
-(defn get-positions [claim]
-  (let [[_ _ starting-position dimension] (str/split claim #":? ")
-        [x y] (map read-string (str/split starting-position #","))
-        [width height] (map read-string (str/split dimension #"x"))
-        x-range (range x (+ x width))
-        y-range (range y (+ y height))]
-    (for [x x-range y y-range]
-      (vector x y))))
+(defn parse-line [line]
+  (->>
+    (re-seq #"\d+" line)
+    (map #(Integer/parseInt %))))
 
-(deftest find-all-the-positions-for-given-claim
-  (testing "find all the positions for a given claim"
-    (let [positions [[3 1] [3 2] [4 1] [4 2]]]
-      (= positions (get-positions "#1 @ 3,1: 2x2")))))
+(defn read-file [path]
+  (with-open [rdr (io/reader path)]
+    (doall (map parse-line (line-seq rdr)))))
+
+(defn get-indices [[_ x y w h]]
+  (for [i (range w) j (range h)] [(+ i x) (+ j y)]))
+
+(defn update-grid [grid claim]
+  (->>
+    (get-indices claim)
+    (reduce #(assoc %1 %2 (inc (get %1 %2 0))) grid)))
+
+(defn part1 [claims data]
+  (count (filter #(> (val %) 1) data)))
+
+;; part 2
+
+(defn check-overlap [claim data]
+  (when (every? #(= 1 (get data %)) (get-indices claim))
+    (first claim)))
+
+(defn part2 [claims data]
+  (some #(check-overlap % data) claims))
+
+(defn -main
+  [& args]
+  (let [claims (read-file "resources/input.txt")
+        data (reduce update-grid {} claims)]
+    (println (part1 claims data))))
